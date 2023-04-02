20505982667cf1eb0bdf260e939643d407dcbc3d
Mayank-Bansal <mayank06570@gmail.com>
2023-04-02 22:52:20 +0530

    Making DashBoard for our Payment App..

--- app/src/main/java/com/example/easepay/DashBoard.java
+++ app/src/main/java/com/example/easepay/DashBoard.java
@@ -1,36 +1,77 @@
 package com.example.easepay;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.Gravity;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NonNls;
 
 public class DashBoard extends AppCompatActivity {
 
-    Button LogOut;
+    DrawerLayout drawerLayout;
     FirebaseAuth mAuth;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
-        LogOut = findViewById(R.id.LogOut_Button);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         mAuth = FirebaseAuth.getInstance();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
 
-        LogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuth.signOut();
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.searchbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this , drawerLayout , toolbar,
+                R.string.navigation_drawer_open , R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+         @SuppressLint("NonConstantResourceId") BottomNavigationView.OnNavigationItemSelectedListener navListener =
+                item -> {
+                    Fragment selectFragment = null;
+                    switch (item.getItemId())
+                    {
+                        case R.id.nav_home:
+                            selectFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_mall:
+                            selectFragment = new MallFragment();
+                            break;
+                        case R.id.nav_bank:
+                            selectFragment = new BankFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectFragment).commit();
+                    return true;
+                };
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+            super.onBackPressed();
     }
 }
